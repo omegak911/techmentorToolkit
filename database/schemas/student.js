00f858f470858833509a3f0dea9d79fdd2ef0624
Kin Chan <xkinchanx@gmail.com>
2018-11-16 19:36:31 -0800

    [prog] adding strict: false, so we can modify the object and add new properties upon update

--- database/schemas/student.js
+++ database/schemas/student.js
@@ -1,42 +1,47 @@
 import db from '../index';
 import mongoose from 'mongoose';
 
-const studentSchema = new mongoose.Schema({
-  name: String,
-  cohort: Number,
-  questions: {
-    JSFundamentals: {
-      average: {
-        type: Number,
-        default: 0
-      }
-    }, //ideally, we have the question # as key and how well they did out of 10
-    CSS: {
-      average: {
-        type: Number,
-        default: 0
-      }
-    },
-    HTML: {
-      average: {
-        type: Number,
-        default: 0
-      }
-    },
-    Database: {
-      average: {
-        type: Number,
-        default: 0
-      }
-    },
-    BH: {
-      average: {
-        type: Number,
-        default: 0
-      }
+const studentSchema = new mongoose.Schema(
+  {
+    name: String,
+    cohort: Number,
+    questions: {
+      JSFundamentals: {
+        average: {
+          type: Number,
+          default: 0
+        }
+      }, //ideally, we have the question # as key and how well they did out of 10
+      CSS: {
+        average: {
+          type: Number,
+          default: 0
+        }
+      },
+      HTML: {
+        average: {
+          type: Number,
+          default: 0
+        }
+      },
+      Database: {
+        average: {
+          type: Number,
+          default: 0
+        }
+      },
+      BH: {
+        average: {
+          type: Number,
+          default: 0
+        }
+      },
     },
   },
-});
+  {
+    strict: false
+  }
+);
 
 const Student = mongoose.model('Student', studentSchema);
 
