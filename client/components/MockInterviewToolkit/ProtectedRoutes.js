8f8e8eb0a0187daf3be6d75adb1ff138f466ac74
Kin Chan <xkinchanx@gmail.com>
2018-11-20 10:45:02 -0800

    [prog] parent component to ensure login-protected routes

---
+++ client/components/MockInterviewToolkit/ProtectedRoutes.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+import Navbar from './Nav';
+import Stats from './Stats';
+import CreationLab from './CreationLab';
+import Session from './Session';
+
+class ProtectedRoutes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    //check sessions, if not active, then redirect back to landing
+  }
+
+  render() {
+    let { match } = this.props;
+    //or maybe match.url
+    return (
+      <div>
+        <Navbar />
+        <Route path={`${match.path}/main`} component={Stats}/>
+        <Route path={`${match.path}/create`} component={CreationLab}/>
+        <Route path={`${match.path}/session`} component={Session}/>
+      </div>
+    )
+  }
+}
+
+export default ProtectedRoutes;
