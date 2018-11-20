5bb8c15b3242f7c97b0e3a5abcf5521d204fd59d
Kin Chan <xkinchanx@gmail.com>
2018-11-20 09:13:02 -0800

    [prog] reaching Landing page

--- client/components/MockInterviewToolkit/index.js
+++ client/components/MockInterviewToolkit/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import Navbar from './Nav';
+import { Route } from 'react-router';
+
+import Landing from './Landing';
+import ProtectedRoutes from './ProtectedRouters';
 
 class MockInterviewToolkit extends Component {
   constructor(props) {
@@ -9,39 +12,12 @@ class MockInterviewToolkit extends Component {
     }
   }
 
-  componentDidMount() {
-    //apicall to determine authentication
-    //if not authenticated, redirect to Landing
-  }
-
   render() {
     let { match } = this.props;
     return (
       <div>
-    Login/Create
-      - regular password for login
-      - master password for creation
-
-    - name + password + conditional rendering as security //we want multiple accounts and a master account
-    - password would be in config
-    Stats
-    - keep track of student progress (sessions, scale of how well they did + comments)
-    - tracker to display students with associated ratings + averages
-    Sessions
-    - customize + save clickable questions to a specific student
-      - exclude questions they have already done
-      - save for specific session //that way we can plan ahead
-    Submit
-    - add student
-    - add questions/category
-
-    state object to determine if Nav should be shown
-    {/* <Navbar />
-    <Route exact path={`${match.path}/`} component={Landing}/>
-
-    <Route path={`${match.path}/main`} component={Stats}/>
-    <Route path={`${match.path}/create`} component={CreationLan}/>
-    <Route path={`${match.path}/session`} component={Session}/> */}
+        <Route exact path={`${match.path}/`} component={Landing}/>
+        <Route path="home" component={ProtectedRoutes}/>
       </div>
     )
   }
