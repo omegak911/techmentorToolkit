576ab28154d0904fe629e64a805bb856df4ac6b1
Kin Chan <xkinchanx@gmail.com>
2018-11-19 20:38:06 -0800

    [prog] flushed out login + create user controllers

--- server/controllers/mockInterview/landingCtrl.js
+++ server/controllers/mockInterview/landingCtrl.js
@@ -12,32 +12,63 @@ import {
   updateStudentCollectionHelper
 } from '../../../database/helpers/mentorHelpers';
 
-const addMentor = (req, res) => {
+import {
+  Create_PW,
+  Login_PW
+} from '../../../config';
 
+const addMentor = (req, res) => {
+  let { name, password } = req.body;
+  if (password === Create_PW) {
+    addMentorHelper(name)
+      .then(() => res.status(201).send('success'))
+      .catch(() => res.status(404).send('error'));
+  } else {
+    res.status(404).send('Forbidden')
+  }
 }
 
 const addBoss = (req, res) => {
-
+  let { name, password } = req.body;
+  if (password === Create_PW) {
+    addBossHelper(name)
+      .then(() => res.status(201).send('success'))
+      .catch(() => res.status(404).send('error'));
+  } else {
+    res.status(404).send('Forbidden')
+  }
 }
 
-const loginValidator = (req, res) => {
-
+const loginValidator = (password) => { //sessions should use this
+  return password === Login_PW;
 }
 
 const updateStudentCollection = (req, res) => {
   //provides name and string in the format of 'students.adminLvl[studentId]'
+  let { name, studentId } = req.body;
+  let studentCollection = `students.adminLvl.${studentId}`;
+  updateStudentCollectionHelper(name, studentCollection)
+    .then(() => res.status(201).send('success'))
+    .catch(() => res.status(404).send('error'));
 }
 
 const getEverything = (req, res) => {
-  getAllStudentsHelper()
-    .then(studentData => {
-      getAllQuestionsHelper()
-        .then(questionData => res.status(201).send({ studentData, questionData }))
-        .catch(err => res.status(404).send('error'));
-    })
-    .catch(err => res.status(404).send('error'));
+  if (loginValidator(req.body.password)) {
+    getAllStudentsHelper()
+      .then(studentData => {
+        getAllQuestionsHelper()
+          .then(questionData => res.status(201).send({ studentData, questionData }))
+          .catch(err => res.status(404).send('error'));
+      })
+      .catch(err => res.status(404).send('error'));
+  } else {
+    res.status(404).send('Forbidden')
+  }
 }
 
 export {
+  addMentor,
+  addBoss,
+  updateStudentCollection,
   getEverything,
 };
