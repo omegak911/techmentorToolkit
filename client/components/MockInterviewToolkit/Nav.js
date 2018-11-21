b28d5a308379ca807bee0af8267421ac2942bc7f
Kin Chan <xkinchanx@gmail.com>
2018-11-20 16:25:05 -0800

    [prog] nested react-router appears to be working

--- client/components/MockInterviewToolkit/Nav.js
+++ client/components/MockInterviewToolkit/Nav.js
@@ -4,15 +4,20 @@ import styled from 'styled-components';
 
 const Navbar = () =>
   <StyledNavbar className="flexCenter">
-    <Link to="/main">Stats</Link>
-    <Link to="/create">Stats</Link>
-    <Link to="/session">Stats</Link>
+    <StyledNavOptions><Link to="/main">Stats</Link></StyledNavOptions>
+    <StyledNavOptions><Link to="/create">Create</Link></StyledNavOptions>
+    <StyledNavOptions><Link to="/session">Session</Link></StyledNavOptions>
   </StyledNavbar>
 
 const StyledNavbar = styled.div`
   background-color: black;
   height: 50px;
   width: 100%;
-`
+`;
+
+const StyledNavOptions = styled.div`
+  margin-left: 1%;
+  margin-right: 1%;
+`;
 
 export default Navbar;
