e20bab024d8b66c480ff8afb7b6716e04c600676
Kin Chan <xkinchanx@gmail.com>
2018-11-21 10:34:06 -0800

    [feat] component template to allow mentors to assign students to themselves

---
+++ client/components/MockInterviewToolkit/Components/Admin/Assign.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Assign extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        left side shows assigned students
+        right side shows all other students
+      </div>
+    )
+  }
+}
+
+export default Assign;
