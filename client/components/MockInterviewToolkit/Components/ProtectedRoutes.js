8a32b04d37add935ef045026d7801d13bf1eb05b
Kin Chan <xkinchanx@gmail.com>
2018-11-21 10:35:57 -0800

    [prog] provides function to reload data on update

--- client/components/MockInterviewToolkit/Components/ProtectedRoutes.js
+++ client/components/MockInterviewToolkit/Components/ProtectedRoutes.js
@@ -6,43 +6,63 @@ import Navbar from './Nav';
 import Stats from './Stats';
 import CreationLab from './CreationLab';
 import Session from './Session';
-import Admin from './Admin';
+import Admin from './Admin/Admin';
 
 class ProtectedRoutes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      update: true,
+      studentData: {},
+      mentorData: {},
+      questionData: {},
+      initalLoadComplete: false
     }
   }
 
   componentDidMount() {
     let { history } = this.props;
-    let { update } = this.state;
-    console.log(this.props);
-    if (update) {
-      axios
-        .get('/api/mockInterview/main/')
-        .then(({ data }) => { 
-          console.log(data)
-          this.setState({ update: false }, () => console.log(this.state));
-        })
-        .catch((err) => { 
-          console.error(err);
-          history.push('/mockInterview');
-        });
-    }
+    axios
+      .get('/api/mockInterview/main/')
+      .then(({ data }) => { 
+        console.log(data);
+        let { studentData, mentorData, questionData } = this.state;
+        this.setState({ studentData, mentorData, questionData, initalLoadComplete: true });
+      })
+      .catch((err) => { 
+        console.error(err);
+        history.push('/mockInterview');
+      });
+  }
+
+  updateMentorStudent = () => {
+    console.log('updateMentorStudent')
+    axios
+      .get('/api/mockInterview/main/mentor')
+      .then(({ data }) => {
+        let { adminData, studentData } = data;
+        this.setState({ adminData, studentData }, () => console.log(this.state));
+      })
+      .catch((err) => { 
+        console.error(err);
+      });
   }
 
   render() {
     let { match } = this.props;
+    let { studentData, mentorData, questionData, initalLoadComplete } = this.state;
     return (
       <div>
         <Navbar match={match}/>
         <Route exact path={`${match.path}/`} component={Stats}/>
         <Route path={`${match.path}/create`} component={CreationLab}/>
         <Route path={`${match.path}/session`} component={Session}/>
-        <Route path={`${match.path}/admin`} component={Admin}/>
+        <Route 
+          path={`${match.path}/admin`} 
+          render={() => <Admin
+              studentData={studentData} 
+              mentorData={mentorData} 
+              updateMentorStudent={this.updateMentorStudent}
+              />} />
       </div>
     )
   }
