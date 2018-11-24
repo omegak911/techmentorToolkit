71938e74028162020e7c97c4d1c8cb655c9c97dc
Kin Chan <xkinchanx@gmail.com>
2018-11-24 13:14:39 -0800

    [prog] planning to split data into two arrays before rendering

--- client/components/MockInterviewToolkit/Components/Admin/Assign.js
+++ client/components/MockInterviewToolkit/Components/Admin/Assign.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
 class Assign extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      assigned: [],
+      unassigned: []
     }
   }
 
+  filter = () => {
+    let { adminData, studentData } = this.props;
+    let assigned = [];
+    let unassigned = [];
+    // based on login username, checks adminData against studentData and displays 
+    //pushes to assigned if on adminData
+    //pushes to unassigned otherwise
+
+  }
+
   render() {
+    let { assigned, unassigned } = this.state;
     return (
-      <div>
+      <StyledFlexRow>
         left side shows assigned students
+        <StyledFlexCol>
+
+        </StyledFlexCol>
         right side shows all other students
-      </div>
+        <StyledFlexCol>
+
+        </StyledFlexCol>
+      </StyledFlexRow>
     )
   }
 }
+
+const StyledFlexRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledFlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 export default Assign;
