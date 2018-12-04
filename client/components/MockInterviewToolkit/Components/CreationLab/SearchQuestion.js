e2211ba959164063dc3fac789a48439327f24beb
Kin Chan <xkinchanx@gmail.com>
2018-12-04 08:53:31 -0800

    [feat] search component within CreationLab

---
+++ client/components/MockInterviewToolkit/Components/CreationLab/SearchQuestion.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import Context from '../../Provider/Context';
+import Question from '../ModuleReuse/Question';
+
+class SearchQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  searchQuestions = (e) => {
+    e.preventDefault();
+
+  }
+
+  updateQuery = (e) => {
+    let { value } = e.target;
+    this.setState({ query: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <form action="" onSubmit={this.searchQuestions}>
+          <input type="text" onChange={this.updateQuery}/>
+          <button type="submit"></button>
+        </form>
+        Search Results
+        <Context.Consumer>
+          {(provider) =>
+            provider.state.questionData.map((question, index) =>
+              <Question category={question.category} text={question.text} key={index}/>
+            )
+          }
+        </Context.Consumer>
+      </div>
+    )
+  }
+}
+
+export default SearchQuestion;
