0c39974c3ecacb0b849a3901b03b7a5b7788f755
Kin Chan <xkinchanx@gmail.com>
2018-12-04 13:19:45 -0800

    [prog] Question component displaying questions and onclick - answers

--- client/components/MockInterviewToolkit/Components/ModuleReuse/Question.js
+++ client/components/MockInterviewToolkit/Components/ModuleReuse/Question.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Question = ({ category, text }) =>
-  <div>
-    <div>
-      {category}
-    </div>
-    <div>
-      {text}
-    </div>
-  </div>
+class Question extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showAnswer: false
+    }
+  }
+
+  showAnswer = () => {
+    this.setState({ showAnswer: !this.state.showAnswer });
+  }
+
+  render() {
+    let { answer, category, question } = this.props;
+    let { showAnswer } = this.state;
+    return (
+      <div>
+        <div>
+          Category: {category}
+        </div>
+        <div>
+          Question: {question}
+        </div>
+          <button type="button" onClick={this.showAnswer}>answer</button>
+        {showAnswer && <div>{answer}</div>}
+      </div>
+    )
+  }
+} 
 
 export default Question;
