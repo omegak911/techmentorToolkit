f52c2c281fefebb0e4b0a1c0da0d31a540a0e0bd
Kin Chan <xkinchanx@gmail.com>
2018-12-04 13:07:27 -0800

    [prog] search provides all questions in related field when blank query is provided

--- client/components/MockInterviewToolkit/Components/CreationLab/SearchQuestion.js
+++ client/components/MockInterviewToolkit/Components/CreationLab/SearchQuestion.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Context from '../../Provider/Context';
 import Question from '../ModuleReuse/Question';
+import Categories from '../ModuleReuse/Categories';
 
 class SearchQuestion extends Component {
   constructor(props) {
@@ -10,6 +11,10 @@ class SearchQuestion extends Component {
       category: null,
       query: ''
     }
+  }
+
+  handleCategorySelect = (category) => {
+    this.setState({ category });
   }
 
   searchQuestions = (e) => {
@@ -23,20 +28,30 @@ class SearchQuestion extends Component {
   }
 
   render() {
+    let { category, query } = this.state;
     return (
       <div>
+        <Categories handleCategorySelect={this.handleCategorySelect}/>
         <form action="" onSubmit={this.searchQuestions}>
           <input type="text" onChange={this.updateQuery}/>
-          <button type="submit"></button>
+          <button type="submit">search</button>
         </form>
         Search Results
-        <Context.Consumer>
-          {(provider) =>
-            provider.state.questionData.map((question, index) =>
-              <Question category={question.category} text={question.text} key={index}/>
-            )
-          }
-        </Context.Consumer>
+        {category && <Context.Consumer>
+          {(provider) => {
+            let { organizedQuestionData } = provider.state;
+            console.log(organizedQuestionData, category)
+            let ids = Object.keys(organizedQuestionData[category]);
+            console.log(ids)
+            return ids.map((id, index) => {
+              let currentQuestion = organizedQuestionData[category][id];
+              let { answer, question } = currentQuestion;
+              if (id !== '' && (question.includes(query) || answer.includes(query))) {
+                return <Question category={currentQuestion.category} question={currentQuestion.question} answer={currentQuestion.answer} key={index}/>
+              }
+            })
+          }}
+        </Context.Consumer>}
       </div>
     )
   }
