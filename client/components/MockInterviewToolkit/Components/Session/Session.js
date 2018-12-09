0315028637fb53175f2a98fee06a76f5b1c0425e
Kin Chan <xkinchanx@gmail.com>
2018-12-09 15:49:08 -0800

    [prog] selectable options to swap between session components

--- client/components/MockInterviewToolkit/Components/Session/Session.js
+++ client/components/MockInterviewToolkit/Components/Session/Session.js
@@ -7,18 +7,39 @@ class Session extends Component {
   constructor(props) {
     super(props);
     this.state ={
-
+      selectedStudent: null,
+      selectedSession: null,
+      mode: null
     }
   }
 
+  changeMode = (e) => {
+    this.setState({ mode: e.target.name });
+  }
+
   render() {
+    let { mode, selectedSession, selectedStudent } = this.state;
     return(
       <div>
-        Session
+        Session - displays assign students
 
-        select Session
+        displays sessions created for assigned student
+        {selectedStudent && 
+          <div>
+            displays sessions from student object
+            <button type="button" name="select" onClick={this.changeMode}>select session</button>
+            <button type="button" name="create" onClick={this.changeMode}>create new session</button>
 
-        create Session
+            {mode === 'select' && <SelectedSession selectedStudent={selectedStudent} session={selectedSession}/>}
+            select Session - needs to select student and session before rendering this
+
+            {mode === 'create' && <NewSession selectedStudent={selectedStudent} />}
+            create Session
+
+          </div>
+        }
+
+
       </div>
     )
   }
