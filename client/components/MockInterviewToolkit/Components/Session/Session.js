86d29df24ee6d0049e643697d86c656429ed8343
Kin Chan <xkinchanx@gmail.com>
2018-12-09 16:30:56 -0800

    [fix] needed provider to wrap entire select option to pass down student data to state

--- client/components/MockInterviewToolkit/Components/Session/Session.js
+++ client/components/MockInterviewToolkit/Components/Session/Session.js
@@ -9,48 +9,54 @@ class Session extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      selectedStudent: "pick a student",
+      selectedStudent: {
+        name: "pick a student"
+      },
       selectedSession: null,
       mode: null
     }
   }
 
-  handleSelect = (e) => {
+  selectMode = (e) => {
     let { name, value } = e.target;
     this.setState({ [name]: value });
+  }
+
+  selectStudent = (selectedStudent) => {
+    this.setState({ selectedStudent });
   }
 
   render() {
     let { mode, selectedSession, selectedStudent } = this.state;
     return(
       <div>
-
         <div>
-          <select value={selectedStudent} name="selectedStudent" onChange={this.handleSelect}>
-            <option disabled={true}>pick a student</option>
-            <Context.Consumer>
-              {(provider) => {
-                return provider.state.assignedStudents.map(student =>
-                  <option key={student._id} student={student}>
+          <Context.Consumer>
+            {(provider) =>
+              <select value={selectedStudent.name} onChange={(e) => this.selectStudent(provider.state.assignedStudents[e.target.value])}>
+                <option disabled={true}>pick a student</option>
+                {provider.state.assignedStudents.map((student, i) =>
+                  <option key={student._id} student={student} value={i}>
                     {student.name}
                   </option>
-                )
-              }}
-            </Context.Consumer>
-          </select>
+                )}
+              </select>
+            }
+          </Context.Consumer>
         </div>
 
-        {selectedStudent !== "pick a student" && 
+        {selectedStudent.name !== "pick a student" && 
           <div>
             displays sessions from student object
-            <button type="button" name="mode" value="select" onClick={this.handleSelect}>select session</button>
-            <button type="button" name="mode" value="create" onClick={this.handleSelect}>create new session</button>
-
-            {mode === 'select' && <SelectedSession selectedStudent={selectedStudent} session={selectedSession}/>}
-            select Session - needs to select student and session before rendering this
+            <button type="button" name="mode" value="select" onClick={this.selectMode}>select session</button>
+            <button type="button" name="mode" value="create" onClick={this.selectMode}>create new session</button>
 
             {mode === 'create' && <NewSession selectedStudent={selectedStudent} />}
             create Session
+
+            {mode === 'select' && selectedStudent.session['1'] && <SelectedSession selectedStudent={selectedStudent} session={selectedSession}/>}
+            select Session - needs to select student and session before rendering this
+
 
           </div>
         }
