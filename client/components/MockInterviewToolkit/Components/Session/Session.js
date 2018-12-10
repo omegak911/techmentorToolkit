db7b2c0e67fe1a6691935a67c0d4723e2d6bb33f
Kin Chan <xkinchanx@gmail.com>
2018-12-09 16:13:09 -0800

    [prog] rendering assigned students with select option

--- client/components/MockInterviewToolkit/Components/Session/Session.js
+++ client/components/MockInterviewToolkit/Components/Session/Session.js
@@ -3,32 +3,48 @@ import React, { Component } from 'react';
 import NewSession from './NewSession';
 import SelectedSession from './SelectedSession';
 
+import Context from '../../Provider/Context';
+
 class Session extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      selectedStudent: null,
+      selectedStudent: "pick a student",
       selectedSession: null,
       mode: null
     }
   }
 
-  changeMode = (e) => {
-    this.setState({ mode: e.target.name });
+  handleSelect = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
   }
 
   render() {
     let { mode, selectedSession, selectedStudent } = this.state;
     return(
       <div>
-        Session - displays assign students
 
-        displays sessions created for assigned student
-        {selectedStudent && 
+        <div>
+          <select value={selectedStudent} name="selectedStudent" onChange={this.handleSelect}>
+            <option disabled={true}>pick a student</option>
+            <Context.Consumer>
+              {(provider) => {
+                return provider.state.assignedStudents.map(student =>
+                  <option key={student._id} student={student}>
+                    {student.name}
+                  </option>
+                )
+              }}
+            </Context.Consumer>
+          </select>
+        </div>
+
+        {selectedStudent !== "pick a student" && 
           <div>
             displays sessions from student object
-            <button type="button" name="select" onClick={this.changeMode}>select session</button>
-            <button type="button" name="create" onClick={this.changeMode}>create new session</button>
+            <button type="button" name="mode" value="select" onClick={this.handleSelect}>select session</button>
+            <button type="button" name="mode" value="create" onClick={this.handleSelect}>create new session</button>
 
             {mode === 'select' && <SelectedSession selectedStudent={selectedStudent} session={selectedSession}/>}
             select Session - needs to select student and session before rendering this
