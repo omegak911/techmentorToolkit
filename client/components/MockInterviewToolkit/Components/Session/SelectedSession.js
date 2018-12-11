6823684f0d744d7b5b389c7664d370e7e7a7a733
Kin Chan <xkinchanx@gmail.com>
2018-12-11 11:27:46 -0800

    [prog] rendering session questions with dummy rating form

--- client/components/MockInterviewToolkit/Components/Session/SelectedSession.js
+++ client/components/MockInterviewToolkit/Components/Session/SelectedSession.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Question from '../ModuleReuse/Question';
 
 class SelectedSession extends Component {
   constructor(props) { //selectedStudent + session #
     super(props);
     this.state = {
-      
+      labelValues: [0,1,2,3,4,5,6,7,8,9,10],
+      //objects with [_id] : { category: 'category', score: 'value' }
     }
+  }
+
+  handleQuestionSelect = ({ _id, category }) => {
+    this.setState({ [_id]: { category }});
   }
 
   render() {
     let { selectedStudent, session } = this.props;
+    let { labelValues } = this.state;
     return (
       <div>
-        shows student name + session #
-        Student: {` ${selectedStudent.name}`}
-        Session #: {` ${session}`}
-
-        displays list of questions for that session # (iterate thru student questions and render based on session val)
+        <p>Student: {` ${selectedStudent.name}`}</p>
+        <p>Session #: {` ${session}`}</p>
 
         for each of those questions, have a textbox field + rating
+
+        {selectedStudent.session[session].map(({ _id, answer, category, question }) =>
+          //wrap this with a div
+          <div key={_id} >
+            <Question _id={_id} answer={answer} category={category} question={question} handleQuestionSelect={this.handleQuestionSelect} />
+            <div>
+              <StyledForm action="">
+                {labelValues.map(value => 
+                  <div key={value}>
+                    <input type="radio"/>
+                    <div>{value}</div>
+                  </div>
+                )}
+              </StyledForm>
+            </div>
+          </div>
+          //have the rating and notes here
+        )}
 
       </div>
     )
   }
 }
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: row;
+`
 
 export default SelectedSession;
